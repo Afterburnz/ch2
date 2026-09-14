9e64d4280121f48d72e8903304726dc22241e2bf
Afterburnz <1289840@learn.vsb.bc.ca>
2026-09-14 14:51:48 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,11 @@
+public class Date {
+	public static void main(String[] args) {
+		String day = "Monday";
+		int date = 14;
+		String month = "September";
+		int year = 2026;
+		System.out.println(day + ", " + month + " " + date + ", " + year);
+		System.out.println(day + " " + date + " " + month + " " + year);
+	}
+
+}
